c5d04c75544440fd114bffbe01e85e25c44dec09
xingmin1 <z_xmin@foxmail.com>
2024-12-22 01:01:02 +0800

    [fix] 修改sys_fstat系统调用，支持获取文件状态信息并引入Kstat结构体

--- src/syscall_imp/fs/ctl.rs
+++ src/syscall_imp/fs/ctl.rs
@@ -433,8 +433,88 @@ pub fn syscall_unlinkat(dir_fd: isize, path: *const u8, flags: usize) -> isize {
         .unwrap_or(-1)
 }
 
-pub(crate) fn sys_fstat(fd: i32, statbuf: *mut c_void) -> i32 {
-    let statbuf = statbuf as *mut arceos_posix_api::ctypes::stat;
+#[derive(Debug, Clone, Copy, Default)]
+#[repr(C)]
+#[cfg(not(target_arch = "x86_64"))]
+pub struct Kstat {
+    /// 设备
+    pub st_dev: u64,
+    /// inode 编号
+    pub st_ino: u64,
+    /// 文件类型
+    pub st_mode: u32,
+    /// 硬链接数
+    pub st_nlink: u32,
+    /// 用户id
+    pub st_uid: u32,
+    /// 用户组id
+    pub st_gid: u32,
+    /// 设备号
+    pub st_rdev: u64,
+    /// padding
+    pub _pad0: u64,
+    /// 文件大小
+    pub st_size: u64,
+    /// 块大小
+    pub st_blksize: u32,
+    /// padding
+    pub _pad1: u32,
+    /// 块个数
+    pub st_blocks: u64,
+    /// 最后一次访问时间(秒)
+    pub st_atime_sec: isize,
+    /// 最后一次访问时间(纳秒)
+    pub st_atime_nsec: isize,
+    /// 最后一次修改时间(秒)
+    pub st_mtime_sec: isize,
+    /// 最后一次修改时间(纳秒)
+    pub st_mtime_nsec: isize,
+    /// 最后一次改变状态时间(秒)
+    pub st_ctime_sec: isize,
+    /// 最后一次改变状态时间(纳秒)
+    pub st_ctime_nsec: isize,
+}
 
-    unsafe { arceos_posix_api::sys_fstat(fd, statbuf) }
+impl From<arceos_posix_api::ctypes::stat> for Kstat {
+    fn from(stat: arceos_posix_api::ctypes::stat) -> Self {
+        Self {
+            st_dev: stat.st_dev,
+            st_ino: stat.st_ino,
+            st_mode: stat.st_mode,
+            st_nlink: stat.st_nlink,
+            st_uid: stat.st_uid,
+            st_gid: stat.st_gid,
+            st_rdev: stat.st_rdev,
+            _pad0: 0,
+            st_size: stat.st_size as u64,
+            st_blksize: stat.st_blksize as u32,
+            _pad1: 0,
+            st_blocks: stat.st_blocks as u64,
+            st_atime_sec: stat.st_atime.tv_sec as isize,
+            st_atime_nsec: stat.st_atime.tv_nsec as isize,
+            st_mtime_sec: stat.st_mtime.tv_sec as isize,
+            st_mtime_nsec: stat.st_mtime.tv_nsec as isize,
+            st_ctime_sec: stat.st_ctime.tv_sec as isize,
+            st_ctime_nsec: stat.st_ctime.tv_nsec as isize,
+        }
+    }
+}
+
+pub(crate) fn sys_fstat(fd: i32, kstatbuf: *mut c_void) -> i32 {
+    let kstatbuf = kstatbuf as *mut Kstat;
+    let mut statbuf = arceos_posix_api::ctypes::stat::default();
+
+    if unsafe {
+        arceos_posix_api::sys_fstat(fd, &mut statbuf as *mut arceos_posix_api::ctypes::stat)
+    } < 0
+    {
+        return -1;
+    }
+
+    unsafe {
+        // Kstat 比 stat 在 st_rdev 与 st_size 多了一个 _pad0 字段
+        let kstat = Kstat::from(statbuf);
+        kstatbuf.write(kstat);
+    }
+    0
 }
